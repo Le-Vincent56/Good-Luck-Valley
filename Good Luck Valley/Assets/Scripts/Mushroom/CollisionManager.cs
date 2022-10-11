08f7909fcbcd3a74d9308724a21975b10da55bc4
Crytonical <Brewster0503@gmail.com>
2022-10-11 15:53:13 -0400

    quad tree started

---
+++ Good Luck Valley/Assets/Scripts/Mushroom/CollisionManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class CollisionManager : MonoBehaviour
+{
+
+    private QuadTreeNode shroomQuad;
+    private QuadTreeNode quadTree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
