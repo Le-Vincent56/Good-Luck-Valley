3ebfbbf572471d23637842d2f2c64a138bd2f760
Owen Bauman <opb5638@rit.edu>
2022-10-15 18:14:54 -0700

    GLV

--- Good Luck Valley/Assets/Scripts/Platform Scripts/WeightedPlatform.cs
+++ Good Luck Valley/Assets/Scripts/Platform Scripts/WeightedPlatform.cs
@@ -5,35 +5,56 @@ using UnityEngine;
 public class WeightedPlatform : MonoBehaviour
 {
     [SerializeField] int requiredWeight;
-    [SerializeField] float maxDis;
-    float initialPosition;
+    [SerializeField] Vector2 direction;
+    [SerializeField] float speed;
+    [SerializeField] float maxDistance;
+    Vector2 initialPosition;
+    Vector2 platformPosition;
+    Vector2 moveDirection;
+    Vector2 velocity;
     bool isMoving;
 
 
     void Start()
     {
-        initialPosition = transform.position.y;
+        initialPosition = transform.position;
+        Debug.Log(initialPosition);
         isMoving = false;
+        platformPosition = new Vector2(transform.position.x, transform.position.y);
     }
 
     void Update()
     {
+        Debug.Log(platformPosition);
+        Debug.Log(Mathf.Abs(initialPosition.y + maxDistance));
+        Debug.Log(Mathf.Abs(platformPosition.y));
+
         if (isMoving == true)
         {
-            transform.position = new Vector3 (transform.position.x, transform.position.y - 1f, 0); // Updates platforms postions to move it down.
+            // What direction platfrom will move in
+            moveDirection = direction;
 
-            if (transform.position.y == initialPosition - maxDis) // Reached its max distance platform can move.
+            // Calculating how much time it will take platfrom to travel to new position
+            velocity = moveDirection * speed * Time.deltaTime;
+
+            // Telling platfrom how to move
+            platformPosition += velocity;
+
+           // Setting the platfroms original position to new position (moving platfrom).
+            transform.position = platformPosition;
+
+            if (Mathf.Abs((int)platformPosition.y) == Mathf.Abs((int)initialPosition.y - maxDistance)) // Platfrom has reached the max distance it can move
             {
-                isMoving = false; // Stop moving.
+                isMoving = false; // Stop moving
             }
         }
     }
 
     /// <summary>
-    /// Checks if the required amount of shroom to move the platform is on it.
+    /// Checks if the required amount of shroom to move the platform is on it
     /// </summary>
     /// <param name="shrooms"></param>
-    void CheckWeight(int shrooms)
+    public void CheckWeight(int shrooms)
     {
         if (requiredWeight == shrooms)
         {
