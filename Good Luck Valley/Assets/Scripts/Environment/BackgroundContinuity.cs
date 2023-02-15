00c679635f71ddd4d63cf1649b421ebac3dbc489
X D <brodster0503@aol.com>
2023-02-14 19:14:48 -0500

    BackgroundContinuity WIP

---
+++ Good Luck Valley/Assets/Scripts/Environment/BackgroundContinuity.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundContinuity : MonoBehaviour
+{
+    [SerializeField]
+    float translateSpeed;
+    [SerializeField]
+    float upperBound;
+    [SerializeField]
+    float lowerBound;
+    PlayerMovement player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
