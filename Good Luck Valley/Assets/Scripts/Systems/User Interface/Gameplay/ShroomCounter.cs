fe63190733485229a0f2de20ebae47472fe5ff38
X D <brodster0503@aol.com>
2023-03-25 17:53:38 -0400

    Fixed clouds, made some changes to the background layers, made shroom count UI more clear

--- Good Luck Valley/Assets/Scripts/Systems/User Interface/Gameplay/ShroomCounter.cs
+++ Good Luck Valley/Assets/Scripts/Systems/User Interface/Gameplay/ShroomCounter.cs
@@ -9,6 +9,12 @@ public class ShroomCounter : MonoBehaviour
     private SpriteRenderer shroomIcon2;
     private SpriteRenderer shroomIcon3;
     private MushroomManager mushMan;
+    private float originalR;
+    private float originalG;
+    private float originalB;
+    private float newR;
+    private float newG;
+    private float newB;
     #endregion
 
     // Start is called before the first frame update
@@ -18,42 +24,56 @@ public class ShroomCounter : MonoBehaviour
         shroomIcon2 = GameObject.Find("Shroom Icon 2").GetComponent<SpriteRenderer>();
         shroomIcon3 = GameObject.Find("Shroom Icon 3").GetComponent<SpriteRenderer>();
         mushMan = GameObject.Find("Mushroom Manager").GetComponent<MushroomManager>();
+        originalR = shroomIcon1.color.r;
+        originalG = shroomIcon1.color.g;
+        originalB = shroomIcon1.color.b;
+        newR = shroomIcon1.color.r - .5f;
+        newG = shroomIcon1.color.g - .5f;
+        newB = shroomIcon1.color.b - .5f;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Check the amount of Mushrooms placed
-        switch (mushMan.MushroomCount)
+        if (mushMan.ThrowUnlocked)
         {
-            // If 0, show all the shroom icons
-            case 0:
-                shroomIcon1.color = new Color(shroomIcon1.color.r, shroomIcon1.color.g, shroomIcon1.color.b, .8f);
-                shroomIcon2.color = new Color(shroomIcon2.color.r, shroomIcon2.color.g, shroomIcon2.color.b, .8f);
-                shroomIcon3.color = new Color(shroomIcon3.color.r, shroomIcon3.color.g, shroomIcon3.color.b, .8f);
-                break;
+            switch (mushMan.MushroomCount)
+            {
+                // If 0, show all the shroom icons
+                case 0:
+                    shroomIcon1.color = new Color(originalR, originalG, originalB, 1f);
+                    shroomIcon2.color = new Color(originalR, originalG, originalB, 1f);
+                    shroomIcon3.color = new Color(originalR, originalG, originalB, 1f);
+                    break;
 
-            // If 1, show two shroom icons
-            case 1:
-                shroomIcon1.color = new Color(shroomIcon1.color.r, shroomIcon1.color.g, shroomIcon1.color.b, .8f);
-                shroomIcon2.color = new Color(shroomIcon2.color.r, shroomIcon2.color.g, shroomIcon2.color.b, .8f);
-                shroomIcon3.color = new Color(shroomIcon3.color.r, shroomIcon3.color.g, shroomIcon3.color.b, 0f);
-                break;
+                // If 1, show two shroom icons
+                case 1:
+                    shroomIcon1.color = new Color(originalR, originalG, originalB, 1f);
+                    shroomIcon2.color = new Color(originalR, originalG, originalB, 1f);
+                    shroomIcon3.color = new Color(newR, newG, newB, .6f);
+                    break;
 
-            // If 2, show one shroom icon
-            case 2:
-                shroomIcon1.color = new Color(shroomIcon1.color.r, shroomIcon1.color.g, shroomIcon1.color.b, .8f);
-                shroomIcon2.color = new Color(shroomIcon2.color.r, shroomIcon2.color.g, shroomIcon2.color.b, 0f);
-                shroomIcon3.color = new Color(shroomIcon3.color.r, shroomIcon3.color.g, shroomIcon3.color.b, 0f);
-                break;
+                // If 2, show one shroom icon
+                case 2:
+                    shroomIcon1.color = new Color(originalR, originalG, originalB, 1f);
+                    shroomIcon2.color = new Color(newR, newG, newB, .6f);
+                    shroomIcon3.color = new Color(newR, newG, newB, .6f);
+                    break;
 
-            // If 3, show none of the shroom icons
-            case 3:
-                shroomIcon1.color = new Color(shroomIcon1.color.r, shroomIcon1.color.g, shroomIcon1.color.b, 0f);
-                shroomIcon2.color = new Color(shroomIcon2.color.r, shroomIcon2.color.g, shroomIcon2.color.b, 0f);
-                shroomIcon3.color = new Color(shroomIcon3.color.r, shroomIcon3.color.g, shroomIcon3.color.b, 0f);
-                break;
+                // If 3, show none of the shroom icons
+                case 3:
+                    shroomIcon1.color = new Color(newR, newG, newB, .6f);
+                    shroomIcon2.color = new Color(newR, newG, newB, .6f);
+                    shroomIcon3.color = new Color(newR, newG, newB, .6f);
+                    break;
+            }
         }
-
+        else
+        {
+            shroomIcon1.color = new Color(newR, newG, newB, 0f);
+            shroomIcon2.color = new Color(newR, newG, newB, 0f);
+            shroomIcon3.color = new Color(newR, newG, newB, 0f);
+        }
     }
 }
