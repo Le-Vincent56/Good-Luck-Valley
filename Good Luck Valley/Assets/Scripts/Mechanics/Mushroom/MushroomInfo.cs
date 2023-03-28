46781d4162c84c6a9220e95d137c2cbc30541947
X D <brodster0503@aol.com>
2023-03-28 01:38:59 -0400

    Shroom stick bug free!

--- Good Luck Valley/Assets/Scripts/Mechanics/Mushroom/MushroomInfo.cs
+++ Good Luck Valley/Assets/Scripts/Mechanics/Mushroom/MushroomInfo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MushroomInfo : MonoBehaviour
 {
     #region REFERENCES
-    public GameObject mushroom;
+
     #endregion
 
     #region FIELDS
@@ -16,6 +16,8 @@ public class MushroomInfo : MonoBehaviour
     private float bouncingTimer = 0.1f;
     private bool onScreen;
     [SerializeField] private bool isShroom;
+    public List<Vector2> colliderPoints;
+    float pointDistance;
     #endregion
 
     #region PROPERTIES
@@ -26,10 +28,25 @@ public class MushroomInfo : MonoBehaviour
     public bool IsShroom { get { return isShroom; } set { isShroom = value; } }
     #endregion
 
+    private void Start()
+    {
+        pointDistance = GetComponent<CircleCollider2D>().radius;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        colliderPoints.Clear();
+        colliderPoints.Add(new Vector2(transform.position.x, transform.position.y + pointDistance));                  // top
+        colliderPoints.Add(new Vector2(transform.position.x, transform.position.y - pointDistance));                  // bottom
+        colliderPoints.Add(new Vector2(transform.position.x + pointDistance, transform.position.y));                  // right
+        colliderPoints.Add(new Vector2(transform.position.x - pointDistance, transform.position.y));                  // left
+
+        colliderPoints.Add(new Vector2(transform.position.x + pointDistance * Mathf.Cos(45), transform.position.y + pointDistance * Mathf.Sin(45)));  // top right
+        colliderPoints.Add(new Vector2(transform.position.x - pointDistance * Mathf.Cos(45), transform.position.y + pointDistance * Mathf.Sin(45)));  // top left
+        colliderPoints.Add(new Vector2(transform.position.x + pointDistance * Mathf.Cos(45), transform.position.y - pointDistance * Mathf.Sin(45)));  // bottom right
+        colliderPoints.Add(new Vector2(transform.position.x - pointDistance * Mathf.Cos(45), transform.position.y - pointDistance * Mathf.Sin(45)));  // bottom left
+
         if (bouncing)
         {
             bouncingTimer -= Time.deltaTime;
