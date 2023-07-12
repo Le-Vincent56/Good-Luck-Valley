74cd3d8e30ae8967a6a0deb3f9f7c78641a2672b
Brody Brewster <bpb4402@rit.edu>
2023-07-12 11:47:41 -0400

    yipee

---
+++ Good Luck Valley/Assets/Scripts/Mechanics/Player/RunningEffects.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunningEffects : MonoBehaviour
+{
+    #region REFERENCES
+    private PlayerMovement player;
+    #endregion
+
+    #region FIELDS
+    #endregion
+
+    #region PROPERTIES
+    #endregion
+
+    private void Awake()
+    {
+        player = GetComponent<PlayerMovement>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
