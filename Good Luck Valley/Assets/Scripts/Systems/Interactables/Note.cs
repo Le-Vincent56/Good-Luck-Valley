69623912e720f3c8fb8d0e28ad4628fc7c82b346
Brody Brewster <bpb4402@rit.edu>
2023-07-26 14:23:59 -0400

    Added rebind for quick bounce

--- Good Luck Valley/Assets/Scripts/Systems/Interactables/Note.cs
+++ Good Luck Valley/Assets/Scripts/Systems/Interactables/Note.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
-using UnityEditor.TextCore.Text;
 
 public class Note : Interactable, IData
 {
