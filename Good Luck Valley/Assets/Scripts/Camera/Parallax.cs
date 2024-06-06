11c9901e65ac78c2ba6dd7aab58f715dfc18ad5a
Vincent Le <89761643+Le-Vincent56@users.noreply.github.com>
2024-06-06 14:36:32 -0400

    Added parallax, but it could easily be jank

---
+++ Good Luck Valley/Assets/Scripts/Camera/Parallax.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+namespace GoodLuckValley.Cameras
+{
+    public class Parallax : MonoBehaviour
+    {
+        [Header("Fields")]
+        [SerializeField] private float smoothing;
+        [SerializeField] private Transform[] layers;
+        private float[] parallaxScales;
+        private Transform cam;
+        private Vector3 previousCamPos;
+
+        private void Awake()
+        {
+            cam = Camera.main.transform;
+        }
+
+        private void Start()
+        {
+            previousCamPos = cam.position;
+
+            parallaxScales = new float[layers.Length];
+            for(int i = 0; i < layers.Length; i++)
+            {
+                parallaxScales[i] = layers[i].position.z * -1;
+            }
+        }
+
+        private void Update()
+        {
+            for(int i = 0; i < layers.Length; i++)
+            {
+                float parallaxX = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+
+                float targetPosX = layers[i].position.x + parallaxX;
+
+                Vector3 targetPos = new Vector3(targetPosX, layers[i].position.y, layers[i].position.z);
+                layers[i].position = Vector3.Lerp(layers[i].position, targetPos, smoothing * Time.deltaTime);
+            }
+
+            previousCamPos = cam.position;
+        }
+    }
+}
