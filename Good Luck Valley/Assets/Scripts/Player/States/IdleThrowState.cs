584aa7d03de3f3200c74a5ccd3654e093b8a0bcd
Vincent Le <89761643+Le-Vincent56@users.noreply.github.com>
2024-06-14 15:34:43 -0400

    Added throwing animations

---
+++ Good Luck Valley/Assets/Scripts/Player/States/IdleThrowState.cs
@@ -0,0 +1,60 @@
+using GoodLuckValley.Audio.Sound;
+using GoodLuckValley.Player.Control;
+using UnityEngine;
+
+namespace GoodLuckValley.Player.States
+{
+    public class IdleThrowState : BaseState
+    {
+        private readonly PlayerSFXHandler sfx;
+        private float animationTimer;
+        private bool Finished { get => animationTimer <= 0; }
+
+        public IdleThrowState(PlayerController player, Animator animator, PlayerSFXHandler sfx) : base(player, animator)
+        {
+            this.sfx = sfx;
+        }
+
+        public override void OnEnter()
+        {
+            animator.CrossFade(IdleThrowHash, crossFadeDuration);
+            animationTimer = 0.4f;
+
+            // Allow the player to peek
+            player.SetCanPeek(true);
+
+            // Play the throwing sound
+            sfx.SporeThrow();
+
+            // Set throwing again to false
+            player.SetThrowingAgain(false);
+        }
+
+        public override void Update()
+        {
+            // Update timers
+            if (animationTimer > 0)
+                animationTimer -= Time.deltaTime;
+
+            Debug.Log(animationTimer);
+
+            // If the animation is finished, stop throwing
+            if (Finished) player.SetThrow(false);
+        }
+
+        public override void FixedUpdate()
+        {
+            // Calculate velocity
+            player.CalculateVelocity();
+
+            // Handle movement
+            player.HandleMovement();
+        }
+
+        public override void OnExit()
+        {
+            // Set throwing to false
+            player.SetThrow(false);
+        }
+    }
+}
