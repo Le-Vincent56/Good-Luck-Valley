24b6e59c586f08fc35bf06c017d7b91c3b3a09e5
Vincent Le <89761643+Le-Vincent56@users.noreply.github.com>
2024-07-05 14:52:08 -0400

    Re-added changes

--- Good Luck Valley/Assets/Scripts/World/Area Triggers/AreaCollider.cs
+++ Good Luck Valley/Assets/Scripts/World/Area Triggers/AreaCollider.cs
@@ -2,19 +2,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-namespace GoodLuckValley.Entities
+namespace GoodLuckValley.World.AreaTriggers
 {
     [RequireComponent(typeof(BoxCollider2D))]
     public class AreaCollider : MonoBehaviour
     {
-        [Header("Events")]
+        public enum Type
+        {
+            Box,
+            Composite
+        }
+
         public UnityAction<GameObject> OnTriggerEnter;
         public UnityAction<GameObject> OnTriggerExit;
 
-        [Header("References")]
         protected BoxCollider2D entityCollider;
+        protected CompositeCollider2D compositeCollider;
 
-        [Header("Fields")]
+        [SerializeField] private Type colliderType;
         [SerializeField] private LayerMask layerMask;
         [SerializeField] private Bounds bounds;
         [SerializeField] private HashSet<GameObject> currentCollisions = new HashSet<GameObject>();
@@ -22,12 +27,28 @@ namespace GoodLuckValley.Entities
         public Bounds Bounds { get { return bounds; } }
 
 
+        private void Reset()
+        {
+            layerMask.value = 1024;
+        }
+
         private void Awake()
         {
-            // Get the box collider
-            entityCollider = GetComponent<BoxCollider2D>();
+            switch(colliderType)
+            {
+                case Type.Box:
+                    // Get the box collider
+                    entityCollider = GetComponent<BoxCollider2D>();
+                    bounds = entityCollider.bounds;
+                    break;
 
-            bounds = entityCollider.bounds;
+                case Type.Composite:
+                    // Get the box collider
+                    compositeCollider = GetComponent<CompositeCollider2D>();
+                    bounds = compositeCollider.bounds;
+                    break;
+            }
+            
         }
 
         private void Update()
