797f0e91d9621aaf142677e523ab28c255d26f06
Brody Brewster <brewster0503@gmail.com>
2024-07-25 11:50:34 -0700

    Level 2 firefly + lantern progress

--- Good Luck Valley/Assets/Scripts/VFX/FireflyParticleController.cs
+++ Good Luck Valley/Assets/Scripts/VFX/FireflyParticleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
 
+[ExecuteInEditMode]
 public class FireflyParticleController : MonoBehaviour
 {
     private VisualEffect fireflyVFX;
