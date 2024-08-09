0b74cb254962b97bd9d515d6acbe167edc779b2d
Vincent Le <89761643+Le-Vincent56@users.noreply.github.com>
2024-08-09 16:33:50 -0400

    Updated the tutorial so that the Journal teaches interact instead of the Mushroom

--- Good Luck Valley/Assets/Scripts/Journal/JournalPickup.cs
+++ Good Luck Valley/Assets/Scripts/Journal/JournalPickup.cs
@@ -1,3 +1,4 @@
+using GoodLuckValley.Events;
 using GoodLuckValley.World.Interactables;
 using UnityEngine;
 
@@ -5,12 +6,17 @@ namespace GoodLuckValley.Journal.Collectibles
 {
     public class JournalPickup : Collectible
     {
+        [SerializeField] private GameEvent onTeachInteractable;
+
         public override void Interact()
         {
             base.Interact();
 
             // Unlock the journal
             Journal.Instance.Unlock();
+
+            // Teach interactable
+            onTeachInteractable.Raise(this, null);
         }
     }
 }
