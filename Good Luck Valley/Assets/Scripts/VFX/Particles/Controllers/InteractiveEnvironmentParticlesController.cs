de3a779494094b81c86f37878d117e45fe6c745c
Brody Brewster <brewster0503@gmail.com>
2024-09-01 21:48:11 -0400

    blue interactive plants no work

--- Good Luck Valley/Assets/Scripts/VFX/Particles/Controllers/InteractiveEnvironmentParticlesController.cs
+++ Good Luck Valley/Assets/Scripts/VFX/Particles/Controllers/InteractiveEnvironmentParticlesController.cs
@@ -1,4 +1,5 @@
 using GoodLuckValley.VFX;
+using GoodLuckValley.VFX.Particles.Controllers;
 using GoodLuckValley.VFX.ScriptableObjects;
 using System.Collections;
 using System.Collections.Generic;
@@ -28,12 +29,51 @@ public class InteractiveEnvironmentParticlesController : MonoBehaviour
         switch(environmentData.PlantType)
         {
             case SimpleInteractiveEnvironmentTrigger.PlantType.Blue:
+                SendVFXDataToGPU(bluePlantParticleData);
                 Debug.Log("blue");
                 break;
             case SimpleInteractiveEnvironmentTrigger.PlantType.Purple:
+                SendVFXDataToGPU(purplePlantParticleData);
                 Debug.Log("purple");
                 break;
         }
         upwardsBurstParticle.Play();
+    }
+
+    private void SendVFXDataToGPU(VFXBurstParticleData data)
+    {
+        // Spawn data
+        upwardsBurstParticle.SetFloat("SpawnCount", data.spawnCount);
+
+        // Initialize data
+        upwardsBurstParticle.SetFloat("Min X Velocity", data.minXVelocity);
+        upwardsBurstParticle.SetFloat("Max X Velocity", data.maxXVelocity);
+        upwardsBurstParticle.SetFloat("Min Y Velocity", data.minYVelocity);
+        upwardsBurstParticle.SetFloat("Max Y Velocity", data.maxYVelocity);
+        upwardsBurstParticle.SetFloat("Min Lifetime", data.minLifetime);
+        upwardsBurstParticle.SetFloat("Max Lifetime", data.maxLifetime);
+        upwardsBurstParticle.SetFloat("Min Size", data.minSize);
+        upwardsBurstParticle.SetFloat("Max Size", data.maxSize);
+        upwardsBurstParticle.SetFloat("Min Starting Angle", data.minStartingAngle);
+        upwardsBurstParticle.SetFloat("Max Starting Angle", data.maxStartingAngle);
+        upwardsBurstParticle.SetVector3("Min Position Offset", data.minPositionOffset);
+        upwardsBurstParticle.SetVector3("Max Position Offset", data.maxPositionOffset);
+
+        // Update data
+        upwardsBurstParticle.SetFloat("Gravity", data.gravity);
+        upwardsBurstParticle.SetFloat("Min Linear Drag Coefficient", data.minLinearDragCoefficient);
+        upwardsBurstParticle.SetFloat("Max Linear Drag Coefficient", data.maxLinearDragCoefficient);
+        upwardsBurstParticle.SetFloat("Turb Intensity", data.turbulenceIntensity);
+        upwardsBurstParticle.SetFloat("Turb Drag", data.turbulenceDrag);
+        upwardsBurstParticle.SetFloat("Turb Frequency", data.turbulenceFrequency);
+        upwardsBurstParticle.SetFloat("Add Z Angle", data.addZAngle);
+        upwardsBurstParticle.SetFloat("Min Add Angle Scalar", data.minAddAngleScalar);
+        upwardsBurstParticle.SetFloat("Max Add Angle Scalar", data.maxAddAngleScalar);
+
+        // Output data
+        upwardsBurstParticle.SetTexture("MainTex", data.mainTex);
+        upwardsBurstParticle.SetAnimationCurve("Mult Size / Life", data.multiplySizeOverLife);
+        upwardsBurstParticle.SetGradient("Color / Life", data.colorOverLife);
+
     }
 }
