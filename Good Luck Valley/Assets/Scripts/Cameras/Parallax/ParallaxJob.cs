3a9d6b5777278fb282d0ee19c2b6863168c31bfc
Vincent Le <le.vincent56@gmail.com>
2025-01-22 15:30:04 -0500

    Added parallax back in using Jobs and Burst for high performance

---
+++ Good Luck Valley/Assets/Scripts/Cameras/Parallax/ParallaxJob.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+namespace GoodLuckValley.Cameras.Parallax
+{
+    [BurstCompile]
+    public struct ParallaxJob : IJobParallelFor
+    {
+        public NativeArray<ParallaxData> parallaxData;
+        public float3 CameraStartPos;
+        public float3 CameraCurrentPos;
+
+        /// <summary>
+        /// Execute Parallax movement
+        /// </summary>
+        public void Execute(int index)
+        {
+            // Retrieve the data
+            ParallaxData data = parallaxData[index];
+
+            // Set the start position
+            float3 position = data.StartPosition;
+
+            // Check if only moving horizontally
+            if (data.HorizontalOnly)
+                // Only change the x-position
+                position.x += data.Multiplier * (CameraCurrentPos.x - CameraStartPos.x);
+            else
+                // Otherwise, change all dimensions
+                position += data.Multiplier * (CameraCurrentPos - CameraStartPos);
+
+            // Set the current position
+            data.CurrentPosition = position;
+
+            // Set the data
+            parallaxData[index] = data;
+        }
+    }
+}
